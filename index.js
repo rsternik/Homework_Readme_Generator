91deec240cb8b10723bedd8b34f9dc895e29fcb6
rsternik <rsternik.wg@gmail.com>
2021-12-01 20:01:47 -0800

    questions created

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        input: 'input',
+        message: 'Enter project title',
+        name: 'title',
+    },
+
+    {
+        input: 'input',
+        message: 'Enter project description',
+        name: 'description',
+    },
+
+    {
+        input: 'input',
+        message: 'Enter installation instructions',
+        name: 'installation',
+    },
+
+    {
+        input: 'input',
+        message: 'Enter usage information',
+        name: 'usage',
+    },
+
+    {
+
+        type: "list",
+        message: 'Choose your license.',
+        choices: ["choice 1", "choice 2"],
+        name: 'license'
+    },
+
+    {
+        input: 'input',
+        message: 'Enter github your username.',
+        name: 'username',
+    },
+
+    {
+        input: 'input',
+        message: 'Enter your email address.',
+        name: 'email',
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
